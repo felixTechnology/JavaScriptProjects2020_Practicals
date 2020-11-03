0c50604318aab81857b1549e6c1e14fcd811e04f
Felix Ackuayi <fackuayi@payswitch.com.gh>
2020-11-03 18:32:41 +0000

    added features to quizApp (loading quiz data from current Quiz 0-Index,select possible answers, not deselecting radio button before click,etc

--- QuizzApp/script.js
+++ QuizzApp/script.js
@@ -8,7 +8,7 @@
 const quizData = [
 
     {
-        question: 'How are is President Nana Akufo Addo..?',
+        question: 'How old is President Nana Akufo Addo..?',
         a: '10',
         b: '76',
         c: '110',
@@ -49,6 +49,15 @@ const quizData = [
         c: 'Hypertext Markup Language',
         d: 'Helicopter Terminals Motors Lamborginis',
         correct: 'c'
+    },
+
+    {
+        question: `When was year was Ghana's independence`,
+        a: '1987',
+        b: '1960',
+        c: '1957',
+        d: '1907',
+        correct: 'c'
     }
 
 
@@ -56,25 +65,93 @@ const quizData = [
 
 ];
 
+
+const quiz = document.getElementById('quiz');
+const answerEls = document.querySelectorAll('.answer');
+
 /*we need to load the questions from html and we will target them through below*/
 const questionEl = document.getElementById("question");/*4*/
 const a_text = document.getElementById("a_text");/*5*/
-const b_text = document.getElementById("a_text");/*6*/
-const c_text = document.getElementById("a_text");/*7*/
-const  d_text = document.getElementById("a_text");/*8*/
+const b_text = document.getElementById("b_text");/*6*/
+const c_text = document.getElementById("c_text");/*7*/
+const  d_text = document.getElementById("d_text");/*8*/
+const  submitBtn = document.getElementById("submitBtn")
 
 
 /*We add the REACT JS*/
 
 /*Keeping Track of current question*/
 let currentQuiz = 0; //giving it zero index /*1*/
+/*let answer = undefined;*/
+let score =0;
 
 /*load quiz: The function will be called everything user submit*/
 loadQuiz();/*2*/
 
 function loadQuiz() {/*3*/
+
+    deselectAnswers();
+
+    //9. this is getting the quiz data of current question
     const  currentQuizData = quizData[currentQuiz];
 
-   questionEl.innerHTML = currentQuizData.question;
+    //10. passing each question properties to the html <h1></h1>
+    questionEl.innerText = currentQuizData.question; /*11*/
+    a_text.innerText = currentQuizData.a;
+    b_text.innerText = currentQuizData.b;/*13*/
+    c_text.innerText = currentQuizData.c;/*14*/
+    d_text.innerText = currentQuizData.d; /*15*/
+
+   /* currentQuestion++;*/
+}
+
+function getSelected(){
+    //console.log("joy")
+       let answer = undefined;
+    //callback function
+    answerEls.forEach((answerEl) =>{
+       /* console.log(answer.value);*/
+        if (answerEl.checked){
+        /*answer =answerEls.id;*/
+
+             answer = answerEl.id;
+        }
+    });
+
+    return answer;
+}
+
+function  deselectAnswers(){
+    answerEls.forEach((answerEl) => {
+        answerEl.checked =false;
+    });
 
 }
+
+submitBtn.addEventListener("click", ()=>{
+
+
+    const answer = getSelected();
+
+     // console.log(answer);
+
+    if (answer){//checking if checked answer is same as the one in quiz data
+
+        if (answer === quizData[currentQuiz].correct){
+            score++;
+        }
+        currentQuiz++; //if we get answer then we move to next question
+
+        //if current quiz is less than the data load quiz
+        if (currentQuiz < quizData.length)
+        {
+            loadQuiz();
+        }else
+        {
+            //TODO: Show RESULT
+            quiz.innerHTML = `<h2>You answered correctly at ${score}/${quizData.length} questions. </h2>`
+        }
+
+    }
+
+})
