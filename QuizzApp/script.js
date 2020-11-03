4bfd60600c0c1c5457259e9ed240056a1e360e24
Felix Atsu Kofi Ackuayi <felixackuayi@aol.com>
2020-11-03 08:08:56 +0000

    created object of an array in JS file for quiz questions

--- QuizzApp/script.js
+++ QuizzApp/script.js
@@ -1,59 +1,57 @@
-/*We need the following
- * 1. Ending date
- * 2. current date
- * 3. subtract to find how many days,mins,hours,and seconds
- * */
-
-/*
-* days
-* hours
-* mins
-* seconds
+/*An object in  array with key-value pair
+* 2. Have box with the the questions in it (https://uidesigndaily.com/posts/sketch-questionnaire-choice-submit-day-924)
+* 2. when we select we store the selected as an id
+* 3. And check if the ID is correct
+*
 * */
 
- const daysEl = document.getElementById('days');/*12*/
- const  hoursEl = document.getElementById('hours');/*13*/
- const  minsEl = document.getElementById('mins');/*14*/
- const  secondsEl = document.getElementById('seconds');/*15*/
+const quizData = [
 
-const newYears = '7 Dec 2020';/*1*/
+    {
+        question: 'How are is President Nana Akufo Addo..?',
+        a: '10',
+        b: '76',
+        c: '110',
+        d: '71',
+        correct: 'b'
+    },
 
-/*Have your function countdown*/
+    {
+        question: 'What is the most used programming language in 2020 ....',
+        a: 'Python',
+        b: 'Java',
+        c: 'JavaScript',
+        d: 'C#',
+        correct: 'a'
+    },
 
-function countdown() {
-    const newYearsDate = new Date(newYears); /*3.passing the string declared into the instance object Date*/
-    const currentDate = new Date; /*2. current date*/
+    {
+        question: 'Where is Amalitech Training Academy Located in Ghana',
+        a: 'Accra',
+        b: 'Tema',
+        c: 'Takoradi',
+        d: 'Cape Coast',
+        correct: 'c'
+    },
+    {
+        question: 'What year was JavaScript Launched',
+        a: '1996',
+        b: '1993',
+        c: '1997',
+        d: 'None of the Above',
+        correct: 'd'
+    },
 
-    const totalSeconds = (newYearsDate - currentDate) /1000; /*7*/
-    /*const days = (seconds /3600/24)*/
-    const days = Math.floor(totalSeconds / 3600 /24);/*8. */
+    {
+        question: 'What does HTML stands for ',
+        a: 'Cascading Style Sheet',
+        b: 'Jason Object Notation',
+        c: 'Hypertext Markup Language',
+        d: 'Helicopter Terminals Motors Lamborginis',
+        correct: 'c'
+    }
 
-    const hours = Math.floor(totalSeconds / 3600 ) % 24;/*9*/
-
-    const minutes = Math.floor(totalSeconds / 60) % 60;/*10*/
-
-    const  seconds = Math.floor(totalSeconds )% 60;/*11*/
 
 
-   /* daysEl.innerHTML =days; /!*12*!/
-    hoursEl.innerHTML =hours;/!*13*!/
-    minsEl.innerHTML = minutes;/!*14*!/
-    secondsEl.innerHTML =seconds;/!*15*!/*/
 
-    daysEl.innerHTML = days; /*12*/
-    hoursEl.innerHTML =formatTime(hours);/*13*/
-    minsEl.innerHTML = formatTime(minutes);/*14*/
-    secondsEl.innerHTML = formatTime(seconds);/*15*/
-
-    //console.log(days, hours,minutes,seconds); /*4.logging in browser to check output..this will give us in millisecond 5100606000*/
-
-}
-
-function formatTime(time) { /*16*/
-
-    return time < 10 ? (`0${time}`) : time; /*17 if we have less than 10 seconds remaining we display 0 */
-
-}
-countdown();/*5*/
-setInterval(countdown, 1000); /*6.This will call JS function/method to call the countdown every second */
-
+]
