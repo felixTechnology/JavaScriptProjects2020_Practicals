26a4f8d6b6993345272552f86bd809e38de5c9ce
Felix Atsu Kofi Ackuayi <felixackuayi@aol.com>
2020-11-04 23:39:46 +0000

    adding JS to make the HTML interractive-created functions

--- MealRecipe-App/script.js
+++ MealRecipe-App/script.js
@@ -4,3 +4,23 @@
 * 3. Search that will give meal based on ID
 *
 * */
+
+
+
+/*Random Meal Function - https://www.themealdb.com/api.php*/
+
+function getRandomMeal() {
+   fetch('https://www.themealdb.com/api/json/v1/1/random.php\n')
+}
+
+
+/*Get Meal by ID*/
+
+function getMealById(id) {
+    fetch('https://www.themealdb.com/api/json/v1/1/lookup.php?i=52772\n' + id)
+
+}
+
+ function getMealsBySearch(term) {
+    fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata\n' + term)
+}
